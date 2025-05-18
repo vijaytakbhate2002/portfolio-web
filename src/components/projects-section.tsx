46cd0beadd33c467c49856db0a480b753d66b776
Vijay Takbhate <vijaytakbhate20@gmail.com>
2025-05-18 23:37:32 +0530

    images added

--- src/components/projects-section.tsx
+++ src/components/projects-section.tsx
@@ -59,41 +59,41 @@ const ProjectsSection: React.FC = () => {
       title: "Healthy Fast Foods: KMeans and Visualization",
       description: "In This project, I explored healthy fast foods and clustered them into three groups based on calorie count. Using these clusters, I identified the healthiest fast food category. This will help people avoid harmful fast foods.",
       tags: ["Unsupervised Learning", "EDA", "Kaggle Notebook"],
-      imageUrl: "https://github.com/user-attachments/assets/aa2e5050-ef42-4c74-b331-664db4fbe968",
-      demoUrl: "#",
-      codeUrl: "#"
+      imageUrl: "https://raw.githubusercontent.com/vijaytakbhate2002/portfolio-web/main/public/healthy_fast_food.jpeg",
+      demoUrl: "https://www.kaggle.com/code/vijay20213/healthy-fast-foods-kmeans-and-visualization",
+      codeUrl: "https://www.kaggle.com/code/vijay20213/healthy-fast-foods-kmeans-and-visualization"
     },
     {
       title: "Pneumonia Detection with CNN and ML with 98% Accuracy",
       description: "I trained a CNN model using 17,000 X-ray images to build a model for pneumonia detection. The project includes a website for easy interaction with the model.",
       tags: ["Deep Learning", "Python", "Kaggle Notebook"],
-      imageUrl: "https://github.com/user-attachments/assets/946487b2-3cfc-484e-8768-5854890ea378",
-      demoUrl: "#",
-      codeUrl: "#"
+      imageUrl: "https://raw.githubusercontent.com/vijaytakbhate2002/portfolio-web/main/public/pneumonia_project.jpg",
+      demoUrl: "https://www.kaggle.com/code/vijay20213/pneumonia-detection-with-cnn-and-ml-with-98-acc",
+      codeUrl: "https://github.com/vijaytakbhate2002/pneumonia-detection.git"
     },
     {
       title: "Twitter Post Sentiment Prediction",
       description: "Engineered an ETL pipeline with PySpark and SQL for sentiment analysis using NLP techniques. Monitored performance through MLFlow on Databricks.",
       tags: ["NLP", "PySpark", "MLFlow"],
       imageUrl: "https://images.unsplash.com/photo-1551288049-bebda4e38f71?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=800&h=400",
-      demoUrl: "#",
-      codeUrl: "#"
+      demoUrl: "https://www.linkedin.com/posts/vijay-takbhate-b9231a236_learning-day-31-integrating-database-activity-7264965901945315328-02wc?utm_source=share&utm_medium=member_desktop&rcm=ACoAADrHnx4Ba4HkdcW3hhRgbi-Dj08HJE4l5ZI",
+      codeUrl: "https://github.com/vijaytakbhate2002/sentiment_prediction_python_package.git"
     },
     {
-      title: "Twitter Post Sentiment Prediction",
-      description: "Engineered an ETL pipeline with PySpark and SQL for sentiment analysis using NLP techniques. Monitored performance through MLFlow on Databricks.",
-      tags: ["NLP", "PySpark", "MLFlow"],
-      imageUrl: "https://images.unsplash.com/photo-1551288049-bebda4e38f71?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=800&h=400",
+      title: "🏋️ Predicting Calorie 🚀 | EDA to Submission",
+      description: "his notebook walks through the end-to-end process of building a machine learning model to predict calorie expenditure based on physical activity data. We'll analyze the dataset, explore patterns, and train an efficient model to make accurate predictions.",
+      tags: ["EDA", "Python", "Kaggle Notebook"],
+      imageUrl: "https://www.kaggle.com/code/vijay20213/predicting-calorie-eda-to-submission",
       demoUrl: "#",
       codeUrl: "#"
     },
     {
       title: "Text-Text Chat-Bot",
       description: "Advanced Text-Text Chat-Bot using the NVIDIA API with prompt engineering for diverse functionalities including paraphrasing, grammar correction, and more.",
-      tags: ["LLM", "Flask", "NVIDIA API"],
+      tags: ["LLM", "Flask", "NVIDIA API", "HTML", "CSS"],
       imageUrl: "https://images.unsplash.com/photo-1677442136019-21780ecad995?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=800&h=400",
-      demoUrl: "#",
-      codeUrl: "#"
+      demoUrl: "https://github.com/vijaytakbhate2002/Text-Text-Generator.git",
+      codeUrl: "https://github.com/vijaytakbhate2002/Text-Text-Generator.git"
     },
     {
       title: "Hand Gesture Recognition",
